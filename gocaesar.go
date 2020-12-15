a95caa4c34f60bce45cbc640d34d819f89c344a9
shaycormac <649070493@qq.com>
2020-12-15 22:04:17 +0800

    feat:新版实现方式，代码上更优雅，使用上更麻烦

--- gocaesar.go
+++ gocaesar.go
@@ -6,16 +6,50 @@ import (
 	"strconv"
 )
 
+type Caesar struct {
+	input      string
+	numberStep int
+	letterStep int
+}
+
+func NewCaesar() *Caesar {
+	return new(Caesar)
+}
+
+func (this *Caesar) Input(input string) *Caesar {
+	this.input = input
+	return this
+}
+
+//设置数字步长
+func (this *Caesar) NumberStep(step int) *Caesar {
+	this.numberStep = step
+	return this
+}
+
+//设置字母步长
+func (this *Caesar) LetterStep(step int) *Caesar {
+	this.letterStep = step
+	return this
+}
+
+//同时设置字母和数字步长
+func (this *Caesar) Step(step int) *Caesar {
+	this.numberStep = step
+	this.letterStep = step
+	return this
+}
+
 //加密
-func BeCaesar(input string, nStep, lStep int) string {
-	var result string = ""
-	inputList := SplitString(input)
+func (this *Caesar) BeCaesar() string {
+	var result = ""
+	inputList := splitString(this.input)
 	for _, v := range inputList {
 		if num, err := strconv.Atoi(v); err == nil {
-			newNumber := define.GetNumShift(num, nStep)
+			newNumber := define.GetNumShift(num, this.numberStep)
 			result = fmt.Sprintf("%s%v", result, newNumber)
 		} else {
-			newLetter := define.GetLetShift(v, lStep)
+			newLetter := define.GetLetShift(v, this.letterStep)
 			result = fmt.Sprintf("%s%s", result, newLetter)
 		}
 	}
@@ -23,22 +57,22 @@ func BeCaesar(input string, nStep, lStep int) string {
 }
 
 //解密
-func DeCaesar(input string, nStep, lStep int) string {
-	var result string = ""
-	inputList := SplitString(input)
+func (this *Caesar) DeCaesar() string {
+	var result = ""
+	inputList := splitString(this.input)
 	for _, v := range inputList {
 		if num, err := strconv.Atoi(v); err == nil {
-			newNumber := define.GetDeNumShift(num, nStep)
+			newNumber := define.GetDeNumShift(num, this.numberStep)
 			result = fmt.Sprintf("%s%v", result, newNumber)
 		} else {
-			newLetter := define.GetDeLetShift(v, lStep)
+			newLetter := define.GetDeLetShift(v, this.letterStep)
 			result = fmt.Sprintf("%s%s", result, newLetter)
 		}
 	}
 	return result
 }
 
-func SplitString(input string) []string {
+func splitString(input string) []string {
 	var list = make([]string, 0)
 	for _, v := range []rune(input) {
 		list = append(list, string(v))
--- gocaesar_test.go
+++ gocaesar_test.go
@@ -6,7 +6,7 @@ func TestBeCaesar(t *testing.T) {
 	text := "abcd1234"
 	nStep := 9
 	lStep := 25
-	result := BeCaesar(text, nStep, lStep)
+	result := NewCaesar().Input(text).NumberStep(nStep).LetterStep(lStep).BeCaesar()
 	t.Logf("原文 : %s, 数字位移 : %d,字母位移 : %d", text, nStep, lStep)
 	t.Logf("密文 : %s", result)
 }
@@ -15,7 +15,7 @@ func TestDeCaesar(t *testing.T) {
 	text := "zabc0123"
 	nStep := 9
 	lStep := 25
-	result := DeCaesar(text, nStep, lStep)
+	result := NewCaesar().Input(text).NumberStep(nStep).LetterStep(lStep).DeCaesar()
 	t.Logf("密文 : %s, 数字位移 : %d,字母位移 : %d", text, nStep, lStep)
 	t.Logf("原文 : %s", result)
 }
